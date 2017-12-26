9083506a46901dfbd30fc1160b428eb805855c20
Steven Senkowski <scsenkowski@gmail.com>
2017-12-26 20:47:37 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -23,13 +23,12 @@ class Student
   def self.find_by_name(name)
     sql = <<-SQL
       SELECT *
-      FROM songs
+      FROM students
     SQL
 
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end.first
-    binding.pry
   end
 
   def save
