587563df08bd69e2a8aef0a5a09d8efdfba2152f
Steven Senkowski <scsenkowski@gmail.com>
2017-12-26 20:55:19 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,35 +51,44 @@ class Student
     DB[:conn].execute(sql)
   end
 
-  def self.first_X_students_in_grade_10(grade)
-    sql = <<-SQL
-      SELECT *
-      FROM students
-      WHERE students.grade< 12
-    SQL
+  def self.first_X_students_in_grade_10(number)
+  sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = 10
+    ORDER BY students.id
+    LIMIT ?
+  SQL
 
-    DB[:conn].execute(sql)
+  DB[:conn].execute(sql, number).map do |row|
+    self.new_from_db(row)
+    end
   end
 
   def self.first_student_in_grade_10
     sql = <<-SQL
       SELECT *
       FROM students
-      WHERE students.grade= 12
-      LIMIT 1
+      WHERE grade = 10
+      ORDER BY students.id LIMIT 1
     SQL
 
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
   def self.all_students_in_grade_X(grade)
     sql = <<-SQL
       SELECT *
       FROM students
-      WHERE students.grade= X
+      WHERE grade = ?
+      ORDER BY students.id
     SQL
 
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql, grade).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def save
