3679c949a293097974cded21b9e983c0be61cde2
Steven Senkowski <scsenkowski@gmail.com>
2017-12-26 20:48:32 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,6 +31,14 @@ class Student
     end.first
   end
 
+  def self.count_all_students_in_grade_9
+    sql = <<-SQL
+      SELECT *
+      FROM students
+    SQL
+
+    DB[:conn].execute(sql)
+  end
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
